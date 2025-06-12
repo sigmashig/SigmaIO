5be786b50dde0133bcfa568e4c27100f76e13bc6
Ihor Shevchenko <igor@ishevchenko.net>
2025-06-12 22:17:03 +0300

    Loger as symlink

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "95"
+  #define BUILD_NUMBER "97"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.2.95 - 2025-06-11 21:38:07.370102"
+  #define VERSION "v0.2.97 - 2025-06-12 16:48:45.148549"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.2.95"
+  #define VERSION_SHORT "v0.2.97"
 #endif
