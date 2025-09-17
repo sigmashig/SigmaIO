35edb2123d8ff9c20be0c5295ad82cf9ae2866b9
Ihor Shevchenko <igor@ishevchenko.net>
2025-09-17 11:07:16 +0300

    Add set of PWM methods

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "128"
+  #define BUILD_NUMBER "132"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.2.128 - 2025-06-15 20:48:04.123565"
+  #define VERSION "v0.2.132 - 2025-08-08 19:43:50.195952"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.2.128"
+  #define VERSION_SHORT "v0.2.132"
 #endif
